6b6517a9cf2677e90d65ad7bc27bb638df33977b
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:15:27 +0000

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+      title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: " kfsopfkpwfopf awjefoiawjf ojewofjja"
+    body: " kfsopfkpwfopf awjefoiawjf ojewofjja",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,24 @@ end
   
   puts "5 skills created"
   
-  9.times do |portfolio_items|
+  8.times do |portfolio_items|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_items}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "His palms are sweaty knees weak arms are heavy 
+          There's vomit on his sweater already, mom's spaghetti
+          He's nervous, but on the surface he looks calm and ready to drop bombs
+          But he keeps on forgetting what he wrote down the whole crowd goes so loud
+          He opens his mouth but the words don't come out",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+    )
+end
+
+  1.times do |portfolio_items|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_items}",
+    subtitle: "Angular",
     body: "His palms are sweaty knees weak arms are heavy 
           There's vomit on his sweater already, mom's spaghetti
           He's nervous, but on the surface he looks calm and ready to drop bombs
