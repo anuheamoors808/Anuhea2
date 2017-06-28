0bb3efff4c496927fe42024f3ae567ba34f12210
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 16:26:37 +0000

    Added new parameters for user registration and editing account

--- db/migrate/20170628154348_devise_create_user.rb
+++ db/migrate/20170628154348_devise_create_user.rb
@@ -1,13 +1,13 @@
-class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+class DeviseCreateUser < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      
+
       ## Custom fields
       t.string :name
-
+    
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -37,8 +37,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :user, :email,                unique: true
+    add_index :user, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
