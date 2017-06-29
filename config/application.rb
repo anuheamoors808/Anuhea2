ff647bd9fa823fd606692035a9a16d0c40c6730a
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:54:24 +0000

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,5 @@ Bundler.require(*Rails.groups)
 
 module AnuheaLol
   class Application < Rails::Application
-     config.generators do |g|
-      g.orm             :active_record
-      g.template_engine :erb
-      g.test_framework  :test_unit, fixture: false
-      g.stylesheets     false
-      g.javascripts     true
-    end
   end
 end
